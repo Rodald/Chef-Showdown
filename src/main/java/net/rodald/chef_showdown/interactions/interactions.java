5d1cda020139590e0cab0b80f37d139f453671da
Rodald <poltdavid@icloud.com>
2024-07-12 00:00:00 +0200

    right click example

--- src/main/java/net/rodald/chef_showdown/interactions/interactions.java
+++ src/main/java/net/rodald/chef_showdown/interactions/interactions.java
@@ -1,6 +1,25 @@
 package net.rodald.chef_showdown.interactions;
 
-public class interactions implements Listener{
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
+public class interactions implements Listener {
+
+    public static Material tablet;
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        // Check if the action is right-clicking a block
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            // Check if the clicked block is of a specific type
+            if (event.getClickedBlock() != null && event.getClickedBlock().getType() == Material.GOLD_BLOCK) {
+                // Perform your logic here
+                event.getPlayer().sendMessage("You clicked on a Gold Block!");
+            }
+        }
+    }
 
 }
