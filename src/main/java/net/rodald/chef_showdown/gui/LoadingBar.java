8ed1c730cabd86bbb1c3c4f50f9c4b1acb747e78
Rodald <poltdavid@icloud.com>
2024-07-29 21:41:54 +0200

    removed unused font style + added stick chance constant

--- src/main/java/net/rodald/chef_showdown/gui/LoadingBar.java
+++ src/main/java/net/rodald/chef_showdown/gui/LoadingBar.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class LoadingBar {
     private JavaPlugin plugin;
     private static final String FONT_NAME = "minecraft:loading_bar";
-    private static final String BACKGROUND_FONT_NAME = "loading_screen_background";
+    private static final int STICK_CHANCE = 30;
     private static final String LOADING_CHARS = "\uE100\uE101\uE102\uE103\uE104\uE105\uE106\uE107\uE108\uE109\uE110\uE111\uE112\uE113\uE114\uE115\uE116\uE117\uE118\uE119\uE120\uE121\uE122\uE123\uE124\uE125\uE126\uE127\uE128\uE129\uE130\uE131\uE132\uE133\uE134\uE135\uE136\uE137\uE138\uE139\uE140\uE141\uE142\uE143\uE144\uE145\uE146\uE147\uE148\uE149\uE150\uE151\uE152\uE153\uE154\uE155\uE156\uE157\uE158\uE159\uE160\uE161\uE162\uE163\uE164\uE165\uE166\uE167\uE168\uE169\uE170\uE171\uE172\uE173\uE174\uE175\uE176\uE177\uE178\uE179\uE180\uE181\uE182\uE183\uE184\uE185\uE186\uE187\uE188\uE189\uE190\uE191\uE192\uE193\uE194\uE195\uE196\uE197\uE198\uE199\uE200";
 
     public LoadingBar(JavaPlugin plugin) {
@@ -30,7 +30,7 @@ public class LoadingBar {
                     return;
                 }
 
-                if (random.nextInt(10) < 3) { // 20% chance to "stick" loading bar
+                if (random.nextInt(100) < STICK_CHANCE) { // 20% chance to "stick" loading bar
                     return;
                 }
 
