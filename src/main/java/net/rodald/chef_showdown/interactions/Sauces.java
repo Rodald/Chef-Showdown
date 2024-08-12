20e022f668881518eae90866403fc8de63ae7b17
Rodald <poltdavid@icloud.com>
2024-08-12 23:57:59 +0200

    important comments

--- src/main/java/net/rodald/chef_showdown/interactions/Sauces.java
+++ src/main/java/net/rodald/chef_showdown/interactions/Sauces.java
@@ -5,11 +5,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class Sauces {
+
+    // adds ketchup
     public static void addKetchup(Player player) {
         if (player.getInventory().getItemInMainHand().getType() == Material.BAKED_POTATO) {
             player.getInventory().setItemInMainHand(new ItemStack(Material.COOKED_COD));
         }
     }
+    // adds mayo
 
     public static void addMayo(Player player) {
         if (player.getInventory().getItemInMainHand().getType() == Material.BAKED_POTATO) {
